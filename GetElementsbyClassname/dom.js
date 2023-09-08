0847ee39324aa93657ac618a36bce8d728d3d3da
nabankur2000m <nabankur2000m@gmail.com>
2023-09-08 22:17:00 +0530

    GetElementsbyClassname

---
+++ GetElementsbyClassname/dom.js
@@ -0,0 +1,6 @@
+var items = document.getElementsByClassName('list-group-iten');
+console. log(items);
+console. log(items[2]);
+items[2].textContent = 'items 2';
+items[2]. style. fontweight = 'bold';
+items[2].style.backgroundColor = 'green';
